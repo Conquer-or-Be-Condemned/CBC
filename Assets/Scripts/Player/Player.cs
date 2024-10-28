d99bc5282eb9e88f9d7a1ee7d9ffca5dc3a489c0
hjd5031 <hjd01034215031@gmail.com>
2024-10-29 00:58:32 +0900

    Feat : 플레이어 상하좌우 이동기능
    애니메이터 조작 필요

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,31 +4,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Debug = System.Diagnostics.Debug;
-
 public class Player : MonoBehaviour
 {
-    public Tilemap map;
-    public Animator ani;
     
     [SerializeField] private float moveSpeed = 0.3f;
 
+    public Tilemap map;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private Rigidbody2D _rb;
     private bool _isMoving;
-
+    private Vector3 sumVector;
     private void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
         _isMoving = false;
     }
 
     private void FixedUpdate()
     {
         PlayerMove2();
+        //CheckDirection(sumVector);
     }
-    
-    private void LateUpdate()
+
+    private void Update()
     {
-        //  Animator가 없는 경우를 상정 
-        if (_isMoving == false && ani != null) ani.speed = 0;
+        CheckDirection(sumVector);
     }
+   
+    // private void LateUpdate()
+    // {
+    //     //  Animator가 없는 경우를 상정 
+    //     if (_isMoving == false && _animator != null) _animator.speed = 0;
+    // }
 
     //  살짝 Lerp되며 움직이는 경향이 있음(사용 안 함)
     private void PlayerMove()
@@ -49,30 +59,33 @@ public class Player : MonoBehaviour
     private void PlayerMove2()
     {
         //  반드시 0으로 초기화 시켜야 오류가 생기지 않음
-        Vector3 sumVector = new Vector3(0,0,0);
+        sumVector = new Vector3(0,0,0);
         
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            setArrowkeyinput(1);
+            //setArrowkeyinput(1);
             sumVector += Vector3.up * moveSpeed;
+            
         }   
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            setArrowkeyinput(1);
+            //setArrowkeyinput(1);
             sumVector += Vector3.left * moveSpeed;
         }   
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            setArrowkeyinput(1);
+            //setArrowkeyinput(1);
             sumVector += Vector3.down * moveSpeed;
 
         }   
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            setArrowkeyinput(1);
+            //setArrowkeyinput(1);
             sumVector += Vector3.right * moveSpeed;
         }
-    
+        //Debug.Print(sumVector.ToString());
+        
+        //CheckDirection(sumVector);
         //  조금 복잡하긴 하지만, collider를 사용하지 않고 플레이어가 맵 밖으로 나가지 않도록
         //  설정할 수 있는 최선의 방법 - 양현석
         transform.position = new Vector3(Mathf.Clamp(transform.position.x+sumVector.x, 
@@ -83,15 +96,58 @@ public class Player : MonoBehaviour
                 map.localBounds.max.y - transform.localScale.y / 2)
             , transform.position.z);
     }
-    
+
+    private void CheckDirection(Vector3 dir)
+    {
+        if (dir.x > 0 && dir.y ==0) //오른쪽 방향키
+        {
+            _animator.SetBool("isSide", true);
+            _animator.SetBool("isIdle", false);
+            _animator.SetBool("isUp", false);
+            _spriteRenderer.flipX = true;
+
+        }
+        else if (dir.x < 0&& dir.y ==0) //왼쪽 방향키
+        {
+            _animator.SetBool("isSide", true);
+            _animator.SetBool("isIdle", false);
+            _animator.SetBool("isUp", false);
+            _spriteRenderer.flipX = false;
+
+        }
+        else if (dir.x == 0 && dir.y > 0) //위쪽 방향키
+        {
+            _animator.SetBool("isSide", false);
+            _animator.SetBool("isIdle", false);
+            _animator.SetBool("isUp", true);
+            _spriteRenderer.flipX = false;
+        
+        }
+        else if (dir.x == 0 && dir.y < 0) //아래쪽 방향키
+        {
+            _animator.SetBool("isSide", false);
+            _animator.SetBool("isIdle", true);
+            _animator.SetBool("isUp", false);
+            _spriteRenderer.flipX = false;
+        
+        }
+        else if (dir.x == 0 && dir.y == 0)
+        {
+            _animator.SetBool("isSide", false);
+            _animator.SetBool("isIdle", true);
+            _animator.SetBool("isUp", false);
+            
+        }
+    }
+
     private void setArrowkeyinput(int key)
     {
         //  애니메이션이 구현되지 않았을 경우를 가정하여
-        if (ani != null)
+        if (_animator != null)
         {
-            ani.SetInteger("state",0);
-            ani.speed = 1;
-            ani.SetInteger("state",key);
+            _animator.SetInteger("state",0);
+            _animator.speed = 1;
+            _animator.SetInteger("state",key);
         }
         _isMoving = true;
     }
