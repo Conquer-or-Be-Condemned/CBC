e3355529a39ff2439e5841e5928f98e4cd52d361
yanggogi1423 <yanggogi1423@gmail.com>
2024-10-28 18:42:19 +0900

    Merge : with New files from Main

--- Assets/Scripts/Spawner/MonsterMovement.cs
+++ Assets/Scripts/Spawner/MonsterMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
-    private Vector2 moveDirection;
+    private Vector3 moveDirection;
     private float moveSpeed;
 
     public void Initialize(Vector2 direction, float speed)
@@ -14,6 +14,6 @@ public class MonsterMovement : MonoBehaviour
     private void Update()
     {
         // 설정된 방향으로 이동
-        transform.position += (Vector3)(moveDirection * moveSpeed * Time.deltaTime);
+        transform.position += (moveDirection * moveSpeed * Time.deltaTime);
     }
 }
